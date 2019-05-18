49ac06b2c183cdbf64186366b61f7f52ef75fca6
sruthysusan <49089852+sruthysusan@users.noreply.github.com>
2019-05-18 10:25:01 +0100

    FinalFix1

--- dialog.cpp
+++ dialog.cpp
@@ -15,9 +15,10 @@ Dialog::Dialog(QWidget *parent) :
   connect(ui->speedScrollBar,SIGNAL(actionTriggered(int)),this, SLOT(on_verticalScrollBar_rangeChanged()));
   connect(ui->setRoverRange,SIGNAL(actionTriggered(int)),this, SLOT(on_setRoverRange_rangeChanged()));
   connect(ui->mySerialPort,SIGNAL(readyRead()),this,SLOT(read_StackReceivedData()));
- // connect(timer_, SIGNAL(timeout()),this, SLOT(myLabelUpdateThread()));
-  connect( this,SIGNAL(updateLabelLists()),this,SLOT( myLabelUpdateThread() ) );
-
+  connect(timer_, SIGNAL(timeout()),this, SLOT(liveControl_Rover()));
+  //connect( this,SIGNAL(updateLabelLists()),this,SLOT( myLabelUpdateThread() ) );
+  connect(ui->autoManual,SIGNAL(clicked()),this,SLOT( setDrive_ReadMode() ) );
+  connect(ui->goBtn,SIGNAL(clicked()),this,SLOT( sendOutBluetoothData() ) );
 }
 
 
@@ -25,13 +26,85 @@ Dialog::~Dialog()
 {
     delete ui;
 }
+void Dialog ::liveControl_Rover()
+{
+    static bool reSend =false;
+    if (ui->steerLeft->isDown() || ui->steerRight->isDown())
+    {
+        reSend=true;
+        sendOutBluetoothData();
+    }
+    else if(reSend)
+    {
+        sendOutBluetoothData(); reSend =false;
+    }
+
+}
+
+void Dialog :: sendOutBluetoothData()
+{
+  SendData.resize(6);
+  SendData.insert(0,PACKET_START);
+  SendData.insert(SPEED,roverSpeed);
+  SendData.insert(SET_OBST,ui->setRoverRange->value());
+
+  setDriveValues();
+
+  if(SendData.at(A_M_RSSI) == MANUAL)
+   {
+      if (ui->steerLeft->isDown())
+          SendData.insert(L_R_STR,LEFT_TURN);
+      else if (ui->steerRight->isDown())
+          SendData.insert(L_R_STR,RIGHT_TURN);
+      else
+          SendData.insert(L_R_STR,NO_DIRECTION);
+   }
+  else SendData.insert(L_R_STR,NO_DIRECTION);
+
+   SendData.insert(RXDATASIZE-1,PACKET_STOP);
+
+if(SendData.size() > 6)
+    SendData.resize(6);
+
+   ui->mySerialPort->write(SendData);
+   ui->mySerialPort->waitForBytesWritten(100);
+}
+
+
+
+void Dialog :: setDriveValues()
+{
+    if (ui->autoManual->text() == "MANUAL")
+        SendData.insert(A_M_RSSI,MANUAL);
+    else if (ui->autoManual->text() == "AUTO")
+        SendData.insert(A_M_RSSI,AUTO);
+    else
+        SendData.insert(A_M_RSSI,RSSI_READ);
+}
+
+
+
+void Dialog :: setDrive_ReadMode()
+{
+  if (ui->autoManual->text() == "MANUAL")
+  {
+      ui->autoManual->setText("AUTO");
+  }
+  else if (ui->autoManual->text() == "AUTO")
+  {
+      ui->autoManual->setText("RSSI_READ");   
+  }
+  else
+  {
+      ui->autoManual->setText("MANUAL");     
+  }
+}
 
 
 
 void Dialog:: read_StackReceivedData()
 {
 //https://www.qtcentre.org/threads/68698-Read-data-from-serial-port
-//    *ui->receivedData = ui->mySerialPort->readAll();
 
     Rxinputs.append(ui->mySerialPort->readAll());
 
@@ -48,23 +121,29 @@ void Dialog:: read_StackReceivedData()
 
 }
 
+
+
 void Dialog::on_verticalScrollBar_rangeChanged()
 {
 //https://www.programiz.com/cpp-programming/library-function/cmath/round
-   int speed =round (ui->speedScrollBar->value()/25);
+   roverSpeed =round (ui->speedScrollBar->value()/25);
 //https://stackoverflow.com/questions/7011447/concatenating-two-qstrings-with-an-integer
-   ui->label->setText(QString("SPEED %1").arg(speed));
+   ui->label->setText(QString("SPEED %1").arg(roverSpeed));
 }
+
+
 
 void Dialog::on_setRoverRange_rangeChanged()
 {
-   ui->setRRLabel->setText(QString("ROV_RANG_LMT-> %1").arg(ui->setRoverRange->value()));
+   ui->setRRLabel->setText(QString("ROV_RANG_LMT-> %1").arg(ui->setRoverRange
+                                                            ->value()));
 }
+
+
 
 void Dialog:: myLabelUpdateThread()
  {
      // compute rssi to approx distance
-
 
      char *m = new char[Rxinputs.size()];
      memcpy( m, Rxinputs.data() ,Rxinputs.size());
@@ -74,8 +153,6 @@ void Dialog:: myLabelUpdateThread()
      QString sValue = QChar(int(m[1+i]));
      unsigned char datas;
      memcpy( &datas, sValue.toStdString().c_str() ,sValue.size());
-     qDebug() << datas ;//<<c;
-
      QString g = (ui->roverConsoleLabels[i]->text());
      g.chop( g.size() - ui->roverConsoleLabels_SIZES[i] );   // current str len - initial str len
      ui-> roverConsoleLabels[i]->setText( g + QString::number(datas));
--- dialog.h
+++ dialog.h
@@ -7,7 +7,11 @@
 #define PACKET_START 77
 #define PACKET_STOP  70
 #define RESET 0
+
 enum {RSSI=1,DIST_2_OBST,GEAR,ROV_SPEED};
+enum {SPEED=1,SET_OBST,A_M_RSSI,L_R_STR};
+
+enum{NO_DIRECTION, LEFT_TURN, RIGHT_TURN, AUTO, MANUAL, RSSI_READ};
 
 namespace Ui {
 class Dialog;
@@ -26,6 +30,9 @@ private slots:
     void on_setRoverRange_rangeChanged();
     void read_StackReceivedData();
     void myLabelUpdateThread();
+    void setDrive_ReadMode();
+    void sendOutBluetoothData();
+    void liveControl_Rover();
 
 signals:
     void updateLabelLists();
@@ -34,9 +41,12 @@ signals:
 private:
     Ui::Dialog *ui;
     QTimer *timer_;
-    uint8_t count;
+    uint8_t count,roverSpeed;
     QByteArray Rxinputs;
     bool initSet[RXDATASIZE];
+    QByteArray SendData;
+
+    void setDriveValues();
 
 };
 
